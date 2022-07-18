a192648a64615fdd9364a34d04ab3b2ae6d62db3
cedricwaltercson <cedricwaltercson@hotmail.com>
2022-07-18 19:15:53 +1000

    Update Injector.cpp

--- Injector/Injector.cpp
+++ Injector/Injector.cpp
@@ -21,11 +21,11 @@ VOID startup(LPCTSTR lpApplicationName) {
 		NULL,							// Process handle not inheritable
 		NULL,							// Thread handle not inheritable
 		FALSE,							// Set handle inheritance to FALSE
-		0,								// No creation flags
+		0,							// No creation flags
 		NULL,							// Use parent's environment block
 		NULL,							// Use parent's starting directory 
 		&si,							// Pointer to STARTUPINFO structure
-		&pi								// Pointer to PROCESS_INFORMATION structure (removed extra parentheses)
+		&pi							// Pointer to PROCESS_INFORMATION structure (removed extra parentheses)
 	);
 	// Close process and thread handles. 
 	CloseHandle(pi.hProcess);
@@ -55,14 +55,14 @@ int APIENTRY wWinMain(_In_     HINSTANCE hInstance,
 					  _In_     int       nCmdShow) {
 
 	const wchar_t* process = L"Game.exe";	// name of the process to inject into
-	startup(process);						// launches the process/program
-	int pID = getProcId(process);			// get our process id
+	startup(process);			// launches the process/program
+	int pID = getProcId(process);		// get our process id
 
-	char dll[] = "payload.dll";				// name of the dll file to inject
-	char dllPath[MAX_PATH] = { 0 };			// full path of the dll
+	char dll[] = "payload.dll";		// name of the dll file to inject
+	char dllPath[MAX_PATH] = { 0 };		// full path of the dll
 	GetFullPathNameA(dll, MAX_PATH, dllPath, NULL);
 
-	HANDLE hProcess = OpenProcess(PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION | PROCESS_VM_OPERATION | PROCESS_VM_WRITE | PROCESS_VM_READ, TRUE, pID);
+	HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pID);
 	LPVOID pszLibFileRemote = VirtualAllocEx(hProcess, NULL, strlen(dllPath) + 1, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
 
 	WriteProcessMemory(hProcess, pszLibFileRemote, dllPath, strlen(dllPath) + 1, NULL);
